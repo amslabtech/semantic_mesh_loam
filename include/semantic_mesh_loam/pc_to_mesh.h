43b5bf82593afb65ada276895ba61cb04dff9064
Hibiki1020 <hibikijitaku@gmail.com>
2020-10-16 19:18:39 +0900

    mesh

--- include/semantic_mesh_loam/pc_to_mesh.h
+++ include/semantic_mesh_loam/pc_to_mesh.h
@@ -2,6 +2,12 @@
 #define __PC_TO_MESH_H
 
 #include"ros/ros.h"
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/io/io.h>
+#include <pcl/io/pcd_io.h>
+#include"pcl/point_cloud.h"
+#include"pcl/point_types.h"
+#include"util.h"
 
 namespace semloam{
 	
@@ -14,13 +20,55 @@ namespace semloam{
 
 			void spin();
 
+			void process();
+
+			void load_PCD();
+
+			void classify_pointcloud();
+
+			bool classify(const pcl::PointXYZRGB& point, const color_data& color_id);
+
+			void generate_mesh();
+
+			void pcl_visualize();
+
 		private:
-			std::string file_path = "/home/amsl/catkin_ws/src/semantic_mesh_loam/PCD_file/";
+			std::string file_path = "/home/amsl/catkin_ws/src/semantic_mesh_loam/PCD_data/";
 			std::string file_name = "semantic_mesh_loam";
 			std::string file_type = "ascii"; //ascii or binary
 
 			bool pcl_mesh_visualize_checker = true;
 			bool ros_mesh_visualize_checker = false;
+
+			int load_counter = 0;
+
+			pcl::PointCloud<pcl::PointXYZRGB> cloud_tmp;
+
+			pcl::PointCloud<pcl::PointXYZRGB> unlabeled;
+			pcl::PointCloud<pcl::PointXYZRGB> outlier;
+			pcl::PointCloud<pcl::PointXYZRGB> car;
+			pcl::PointCloud<pcl::PointXYZRGB> bicycle;
+			pcl::PointCloud<pcl::PointXYZRGB> bus;
+			pcl::PointCloud<pcl::PointXYZRGB> motorcycle;
+			pcl::PointCloud<pcl::PointXYZRGB> onrails;
+			pcl::PointCloud<pcl::PointXYZRGB> truck;
+			pcl::PointCloud<pcl::PointXYZRGB> othervehicle;
+			pcl::PointCloud<pcl::PointXYZRGB> person;
+			pcl::PointCloud<pcl::PointXYZRGB> bicyclist;
+			pcl::PointCloud<pcl::PointXYZRGB> motorcyclist;
+			pcl::PointCloud<pcl::PointXYZRGB> road;
+			pcl::PointCloud<pcl::PointXYZRGB> parking;
+			pcl::PointCloud<pcl::PointXYZRGB> sidewalk;
+			pcl::PointCloud<pcl::PointXYZRGB> otherground;
+			pcl::PointCloud<pcl::PointXYZRGB> building;
+			pcl::PointCloud<pcl::PointXYZRGB> fence;
+			pcl::PointCloud<pcl::PointXYZRGB> otherstructure;
+			pcl::PointCloud<pcl::PointXYZRGB> lanemarking;
+			pcl::PointCloud<pcl::PointXYZRGB> vegetation;
+			pcl::PointCloud<pcl::PointXYZRGB> trunk;
+			pcl::PointCloud<pcl::PointXYZRGB> terrain;
+			pcl::PointCloud<pcl::PointXYZRGB> pole;
+			pcl::PointCloud<pcl::PointXYZRGB> trafficsign;
 	};
 }
 
--- src/lib/pctomesh.cpp
+++ src/lib/pctomesh.cpp
@@ -8,7 +8,7 @@ namespace semloam{
 
 	}
 
-	bool PcToMesh::PcToMesh(ros::NodeHandle& node, ros::NodeHandle& privateNode){
+	bool PcToMesh::setup(ros::NodeHandle& node, ros::NodeHandle& privateNode){
 
 		float fparam;
 		int iparam;
@@ -75,6 +75,151 @@ namespace semloam{
 
 		process();
 
+	}
+
+	void PcToMesh::process(){
+
+		load_PCD();
+
+		generate_mesh();
+
+	}
+
+	void PcToMesh::load_PCD(){
+		
+		bool load_checker = true;
+
+		while(load_checker==true){
+			std::string number = std::to_string(load_counter);
+
+			std::string file = file_path + file_name + "_" + file_type + number + ".pcd";
+
+			int load_file = pcl::io::loadPCDFile(file, cloud_tmp);
+
+			if(load_file == 0){//0 means loading pcd file is successfull
+				load_counter += 1;
+
+				classify_pointcloud();
+				
+				//cloud += cloud_tmp;
+				cloud_tmp.clear();
+
+				std::cout << "Loading " << file << " is successfull" << std::endl;
+
+			}
+			else{
+				cloud_tmp.clear();
+				std::cout << file << " is not found" << std::endl;
+
+				load_checker = false;
+			}
+		}
+
+		std::cout << "Loading PCD file has done" << std::endl;
+
+	}
+
+	void PcToMesh::classify_pointcloud(){
+		size_t cloud_size = cloud_tmp.size();
+
+		pcl::PointXYZRGB point;
+
+		for(int i=0; i<cloud_size; i++){
+
+			point.x = cloud_tmp[i].x;
+			point.y = cloud_tmp[i].y;
+			point.z = cloud_tmp[i].z;
+
+			point.r = cloud_tmp[i].r;
+			point.g = cloud_tmp[i].g;
+			point.b = cloud_tmp[i].b;
+
+			color_data color_id;
+			color_id.r = int(point.r);
+			color_id.g = int(point.g);
+			color_id.b = int(point.b);
+
+			if(!classify(point, color_id) ){
+				//
+			}
+		}
+	}
+
+	bool PcToMesh::classify(const pcl::PointXYZRGB& point, const color_data& color_id){
+		if(color_id.r==0 && color_id.g==0 && color_id.b==0){
+			//unlabeled.push_back(point);
+		}
+		else if(color_id.r==255 && color_id.g==0 && color_id.b==0){
+			//outlier.push_back(point);
+		}
+		else if(color_id.r==100 && color_id.g==150 && color_id.b==245){
+			car.push_back(point);
+		}
+		else if(color_id.r==100 && color_id.g==230 && color_id.b==245){
+			bicycle.push_back(point);
+		}
+		else if(color_id.r==100 && color_id.g==80 && color_id.b==250){
+			bus.push_back(point);
+		}
+		else if(color_id.r==30 && color_id.g==60 && color_id.b==150){
+			motorcycle.push_back(point);
+		}
+		else if(color_id.r==0 && color_id.g==0 && color_id.b==255){
+			onrails.push_back(point);
+		}
+		else if(color_id.r==80 && color_id.g==30 && color_id.b==180){
+			truck.push_back(point);
+		}
+		else if(color_id.r==255 && color_id.g==30 && color_id.b==30){
+			//person.push_back(point);
+		}
+		else if(color_id.r==255 && color_id.g==40 && color_id.b==200){
+			//bicyclist.push_back(point);
+		}
+		else if(color_id.r==150 && color_id.g==30 && color_id.b==90){
+			//motorcyclist.push_back(point);
+		}
+		else if(color_id.r==255 && color_id.g==0 && color_id.b==255){
+			road.push_back(point);
+		}
+		else if(color_id.r==255 && color_id.g==150 && color_id.b==255){
+			parking.push_back(point);
+		}
+		else if(color_id.r==75 && color_id.g==0 && color_id.b==75){
+			sidewalk.push_back(point);
+		}
+		else if(color_id.r==175 && color_id.g==0 && color_id.b==75){
+			otherground.push_back(point);
+		}
+		else if(color_id.r==255 && color_id.g==200 && color_id.b==0){
+			building.push_back(point);
+		}
+		else if(color_id.r==255 && color_id.g==120 && color_id.b==50){
+			fence.push_back(point);
+		}
+		else if(color_id.r==255 && color_id.g==150 && color_id.b==0){
+			otherstructure.push_back(point);
+		}
+		else if(color_id.r==150 && color_id.g==255 && color_id.b==170){
+			lanemarking.push_back(point);
+		}
+		else if(color_id.r==0 && color_id.g==175 && color_id.g==0){
+			vegetation.push_back(point);
+		}
+		else if(color_id.r==135 && color_id.g==60 && color_id.b==0){
+			trunk.push_back(point);
+		}
+		else if(color_id.r==150 && color_id.g==240 && color_id.b==80){
+			terrain.push_back(point);
+		}
+		else if(color_id.r==255 && color_id.g==240 && color_id.b==150){
+			pole.push_back(point);
+		}
+		else if(color_id.r==255 && color_id.g==0 && color_id.b==0){
+			trafficsign.push_back(point);
+		}
+
+		return true;
 	}
 
 
